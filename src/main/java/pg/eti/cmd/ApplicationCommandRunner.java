0d1f1b02e6ece1f27cdd2be2d4f7e4ce98fae527
Sladamos <sladamo@wp.pl>
2023-10-07 20:01:27 +0200

    Creating simple books command

--- src/main/java/pg/eti/cmd/ApplicationCommandRunner.java
+++ src/main/java/pg/eti/cmd/ApplicationCommandRunner.java
@@ -3,14 +3,13 @@ package pg.eti.cmd;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import pg.eti.book.entity.Book;
+import pg.eti.book.entity.PublishingHouse;
 import pg.eti.book.service.api.BookService;
 import pg.eti.book.service.api.PublishingHouseService;
 import pg.eti.shutdown.ApplicationShutdownManager;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.UUID;
+import java.util.*;
 
 @Component
 public class ApplicationCommandRunner implements CommandLineRunner {
@@ -46,9 +45,24 @@ public class ApplicationCommandRunner implements CommandLineRunner {
         commands.put("exit", this::disableProgramFunction);
         commands.put("list_all_publishing_houses", () -> publishingHouseService.findAll().forEach(System.out::println));
         commands.put("list_all_books", () -> bookService.findAll().forEach(System.out::println));
-        //commands.put("add_book");
+        commands.put("add_book", () -> {
+            commands.get("list_all_publishing_houses").run();
+            Optional<PublishingHouse> publishingHouse = publishingHouseService.find(UUID.fromString(scanner.next()));
+            if(publishingHouse.isPresent()) {
+                System.out.println("Provide ID, title and author");
+                Book book = Book.builder()
+                        .id(UUID.fromString(scanner.next()))
+                        .title(scanner.nextLine())
+                        .author(scanner.nextLine())
+                        .build();
+                bookService.create(book);
+            } else {
+                System.out.println("Incorrect publishing house id");
+            }
+        });
+
         commands.put("delete_book", () -> {
-            System.out.println("Provide UUID");
+            System.out.println("Provide ID");
             UUID id = UUID.fromString(scanner.next());
             bookService.delete(id);
             System.out.println("Book removed properly.");
