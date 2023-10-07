464499beb5afcd9c0a7c8f15f23f8d8cb0afb09f
Sladamos <sladamo@wp.pl>
2023-10-07 12:31:42 +0200

    PublishingHouseService methods

---
+++ src/main/java/pg/eti/book/service/api/PublishingHouseService.java
@@ -0,0 +1,13 @@
+package pg.eti.book.service.api;
+
+import pg.eti.book.entity.PublishingHouse;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PublishingHouseService {
+
+    Optional<PublishingHouse> find(UUID id);
+
+    void create(PublishingHouse publishingHouse);
+}
