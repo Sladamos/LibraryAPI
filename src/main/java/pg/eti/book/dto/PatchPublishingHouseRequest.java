03a2a0575194c361dbe74a751d0285d894421f7c
Sladamos <sladamo@wp.pl>
2023-10-10 00:15:33 +0200

    Patch and put publishing house DTO

---
+++ src/main/java/pg/eti/book/dto/PatchPublishingHouseRequest.java
@@ -0,0 +1,8 @@
+package pg.eti.book.dto;
+
+public class PatchPublishingHouseRequest {
+
+	private String name;
+
+	private int creationYear;
+}
---
+++ src/main/java/pg/eti/book/dto/PutPublishingHouseRequest.java
@@ -0,0 +1,13 @@
+package pg.eti.book.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class PutPublishingHouseRequest {
+
+	private String name;
+
+	private int creationYear;
+}
