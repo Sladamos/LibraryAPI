5e3d0e384d1e13620bc35d85b0e3e584553358da
Sladamos <sladamo@wp.pl>
2023-10-09 15:43:55 +0200

    Publishing house to response function and response

---
+++ src/main/java/pg/eti/book/function/PublishingHouseToResponseFunction.java
@@ -0,0 +1,16 @@
+package pg.eti.book.function;
+
+import pg.eti.book.dto.GetPublishingHouseResponse;
+import pg.eti.book.entity.PublishingHouse;
+
+import java.util.function.Function;
+
+public class PublishingHouseToResponseFunction implements Function<PublishingHouse, GetPublishingHouseResponse> {
+	@Override
+	public GetPublishingHouseResponse apply(PublishingHouse publishingHouse) {
+		return GetPublishingHouseResponse.builder()
+				.id(publishingHouse.getId())
+				.name(publishingHouse.getName())
+				.build();
+	}
+}
