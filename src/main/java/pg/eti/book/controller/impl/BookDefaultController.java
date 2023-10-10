ac26cd5a51c7cab90675d70a4d54ae8e1a692fa7
Sladamos <sladamo@wp.pl>
2023-10-10 11:54:05 +0200

    Semi created implementation of book controller

---
+++ src/main/java/pg/eti/book/controller/impl/BookDefaultController.java
@@ -0,0 +1,71 @@
+package pg.eti.book.controller.impl;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import pg.eti.book.controller.api.BookController;
+import pg.eti.book.dto.GetBookResponse;
+import pg.eti.book.dto.GetBooksResponse;
+import pg.eti.book.dto.PatchBookRequest;
+import pg.eti.book.dto.PutBookRequest;
+import pg.eti.book.function.BookToResponseFunction;
+import pg.eti.book.function.BooksToResponseFunction;
+import pg.eti.book.service.api.BookService;
+
+import java.util.UUID;
+
+@RestController
+@Log
+public class BookDefaultController implements BookController {
+
+	private final BookService service;
+
+	private final BookToResponseFunction bookToResponse;
+
+	private final BooksToResponseFunction booksToResponse;
+
+	@Autowired
+	public BookDefaultController(
+			BookService service,
+			BookToResponseFunction bookToResponse,
+			BooksToResponseFunction booksToResponse) {
+		this.service = service;
+		this.bookToResponse = bookToResponse;
+		this.booksToResponse = booksToResponse;
+	}
+
+	@Override
+	public GetBooksResponse getBooks() {
+		return booksToResponse.apply(service.findAll());
+	}
+
+	@Override
+	public GetBookResponse getBook(UUID id) {
+		return service.find(id)
+				.map(bookToResponse)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+
+	@Override
+	public void putBook(UUID id, PutBookRequest request) {
+		//service.create();
+	}
+
+	@Override
+	public void patchBook(UUID id, PatchBookRequest request) {
+	//
+	}
+
+	@Override
+	public void deleteBook(UUID id) {
+		service.find(id)
+				.ifPresentOrElse(
+						character -> service.delete(id),
+						() -> {
+							throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+						}
+				);
+	}
+}
