99a46a7635dd1683ca52662e81fe320a8f85bcc7
Sladamos <sladamo@wp.pl>
2023-10-10 22:25:42 +0200

    Request moved to proper package and annotated as component

--- src/main/java/pg/eti/book/function/RequestToPublishingHouseFunction.java
+++ src/main/java/pg/eti/book/function/RequestToPublishingHouseFunction.java
@@ -1,10 +1,13 @@
-package pg.eti.book.dto;
+package pg.eti.book.function;
 
+import org.springframework.stereotype.Component;
+import pg.eti.book.dto.PutPublishingHouseRequest;
 import pg.eti.book.entity.PublishingHouse;
 
 import java.util.UUID;
 import java.util.function.BiFunction;
 
+@Component
 public class RequestToPublishingHouseFunction implements BiFunction<UUID, PutPublishingHouseRequest, PublishingHouse> {
 
 	@Override
