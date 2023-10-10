240d3192b110100cb6dfa933b9cf4c82e6fae900
Sladamos <sladamo@wp.pl>
2023-10-10 11:59:46 +0200

    PutRequestToBookFunction created

---
+++ src/main/java/pg/eti/book/function/PutRequestToBookFunction.java
@@ -0,0 +1,25 @@
+package pg.eti.book.function;
+
+import pg.eti.book.dto.PutBookRequest;
+import pg.eti.book.entity.Book;
+import pg.eti.book.entity.PublishingHouse;
+
+import java.util.UUID;
+import java.util.function.BiFunction;
+
+public class PutRequestToBookFunction implements BiFunction<UUID, PutBookRequest, Book> {
+	@Override
+	public Book apply(UUID id, PutBookRequest putBookRequest) {
+		return Book.builder()
+				.id(id)
+				.author(putBookRequest.getAuthor())
+				.title(putBookRequest.getTitle())
+				.isbn(putBookRequest.getIsbn())
+				.numberOfPages(putBookRequest.getNumberOfPages())
+				.genres(putBookRequest.getGenres())
+				.publishingHouse(PublishingHouse.builder()
+						.id(putBookRequest.getPublishingHouse())
+						.build())
+				.build();
+	}
+}
