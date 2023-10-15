3c01bd6ea067db5816984dbcd64bda7146a0af6a
Sladamos <sladamo@wp.pl>
2023-10-15 13:58:37 +0200

    Preparation for rest service but with bugs

---
+++ library-api-publishing-houses/src/main/java/pg/eti/publishinghouse/event/repository/rest/PublishingHouseEventRestRepository.java
@@ -0,0 +1,37 @@
+package pg.eti.publishinghouse.event.repository.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+import pg.eti.publishinghouse.dto.PatchPublishingHouseRequest;
+import pg.eti.publishinghouse.dto.PutPublishingHouseRequest;
+import pg.eti.publishinghouse.entity.PublishingHouse;
+import pg.eti.publishinghouse.event.repository.api.PublishingHouseEventRepository;
+
+import java.util.UUID;
+
+@Repository
+public class PublishingHouseEventRestRepository implements PublishingHouseEventRepository {
+
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public PublishingHouseEventRestRepository(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public void delete(UUID id) {
+        restTemplate.delete("/api/publishing-houses/{id}", id);
+    }
+
+    @Override
+    public void create(PublishingHouse publishingHouse) {
+        restTemplate.put("/api/publishing-houses/{id}", publishingHouse);
+    }
+
+    @Override
+    public void update(PublishingHouse publishingHouse) {
+        restTemplate.patchForObject("/api/publishing-houses/{id}", publishingHouse);
+    }
+}
