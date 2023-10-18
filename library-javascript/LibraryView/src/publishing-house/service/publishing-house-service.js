60ec50789f552ec17d0be4cec86e089c577e5dc2
Sladamos <sladamo@wp.pl>
2023-10-19 00:26:57 +0200

    Publishing house service implemented

---
+++ library-javascript/LibraryView/src/publishing-house/service/publishing-house-service.js
@@ -0,0 +1,62 @@
+import "../type/types.js";
+
+export class PublishingHouseService {
+  getPublishingHouses() {
+    return fetch("/api/publishing-houses").then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network error.");
+    });
+  }
+
+  deletePublishingHouse(id) {
+    return fetch("/api/publishing-houses/" + id, {
+      method: "DELETE",
+    }).then((response) => {
+      if (response.ok) {
+        return;
+      }
+      throw new Error("Network error.");
+    });
+  }
+
+  getPublishingHouse(id) {
+    return fetch("/api/publishing-houses/" + id).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network error.");
+    });
+  }
+
+  putPublishingHouse(id, request) {
+    return fetch("/api/publishing-houses/" + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    }).then((response) => {
+      if (response.ok) {
+        return;
+      }
+      throw new Error("Network error.");
+    });
+  }
+
+  patchPublishingHouse(id, request) {
+    return fetch("/api/publishing-houses/" + id, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(request),
+    }).then((response) => {
+      if (response.ok) {
+        return;
+      }
+      throw new Error("Network error.");
+    });
+  }
+}
