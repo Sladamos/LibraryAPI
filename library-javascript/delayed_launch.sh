1d81b7dc18de21fe1eafa5350e0d4c687f5c9ca0
Sladamos <sladamos@wp.pl>
2023-11-29 21:06:47 +0100

    Working proxy after editing env variables

--- library-javascript/delayed_launch.sh
+++ library-javascript/delayed_launch.sh
@@ -3,4 +3,5 @@
 DELAY_TIME=$1
 sleep $DELAY_TIME
 envsubst '${API_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+cp /etc/nginx/conf.d/default.conf /etc/nginx/templates/default.conf.template
 nginx -g "daemon off;"
