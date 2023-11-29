343d9b754bbf9d42bbf3764fd6ccb0f72ed78e13
Sladamos <sladamos@wp.pl>
2023-11-29 14:41:56 +0100

    Update build and docker compose for utility container

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 function main() {
+    echo books
     cd ./library-api-books/; sh ./build.sh; cd ..
+    echo houses
     cd ./library-api-publishing-houses/; sh ./build.sh; cd ..
+    echo gateway
     cd ./library-api-gateway/; sh ./build.sh; cd ..
-    cd ./library-javascript/; sh ./build.sh; cd ..
+    echo front
+    docker-compose run --rm npm
 }
 
-main()
+main
