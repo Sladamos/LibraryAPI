8becaddb44624d8211b623ff85dbee7a0f4c5934
Sladamos <sladamos@wp.pl>
2023-12-07 09:52:31 +0100

    Register publishing houses in eureka

--- library-api-publishing-houses/src/main/java/pg/eti/SpringApp.java
+++ library-api-publishing-houses/src/main/java/pg/eti/SpringApp.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringApp {
 
