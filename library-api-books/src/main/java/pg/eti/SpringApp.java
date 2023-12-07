39efcea61bed149f39e358d3e8289e2d7c1bfb4c
Sladamos <sladamos@wp.pl>
2023-12-07 01:19:13 +0100

    Working eureka with two books instances

--- library-api-books/src/main/java/pg/eti/SpringApp.java
+++ library-api-books/src/main/java/pg/eti/SpringApp.java
@@ -2,10 +2,12 @@ package pg.eti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringApp {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(SpringApp.class, args);
     }
